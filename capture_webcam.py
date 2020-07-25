bd8fde5452aff11bd8d4ecf1b80d54b8f6307269
Nik <62773963+niklai99@users.noreply.github.com>
2020-07-25 16:20:19 +0200

    Added some checks to avoid errors

--- capture_webcam.py
+++ capture_webcam.py
@@ -2,9 +2,18 @@ import cv2
 
 cap = cv2.VideoCapture(0)
 
+if not cap.isOpened():
+    print("Cannot open camera")
+    exit()
+
 while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
+
+    # if frame is read correctly ret is True
+    if not ret:
+        print("Can't receive frame.")
+        break
 
     # Our operations on the frame come here
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
