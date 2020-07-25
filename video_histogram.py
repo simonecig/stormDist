3eb1923571a43333f8026a5d05a876f1f92705a3
Nik <62773963+niklai99@users.noreply.github.com>
2020-07-25 17:02:37 +0200

    Added some comments on the video_histogram.py file

--- video_histogram.py
+++ video_histogram.py
@@ -3,40 +3,44 @@ import matplotlib.pyplot as plt
 import argparse
 import cv2
 
+# Using an argument parser for color, bins, and width of the frame
 parser = argparse.ArgumentParser()
 
-parser.add_argument('-c', '--color', type=str, default='gray',
-    help='Color space: "gray" (default), "rgb", or "lab"')
-parser.add_argument('-b', '--bins', type=int, default=16,
-    help='Number of bins per channel (default 16)')
-parser.add_argument('-w', '--width', type=int, default=0,
-    help='Resize video to specified width in pixels (maintains aspect)')
+parser.add_argument('-c', '--color', type=str, default='gray')
+parser.add_argument('-b', '--bins', type=int, default=16)
+parser.add_argument('-w', '--width', type=int, default=0)
 
 args = vars(parser.parse_args())
 
-# Configure VideoCapture class instance for using camera or file input.
-
+# Initialize the video capture
 cap = cv2.VideoCapture(0)
 
+# Define color, bins and width variables
 color = args['color']
 bins = args['bins']
 resizeWidth = args['width']
 
-# Plot
+# Plot configurations
 fig, ax = plt.subplots()
 ax.set_title('Histogram')
 ax.set_xlabel('Bin')
 ax.set_ylabel('Frequency')
 
-# Initialize plot line object(s). Turn on interactive plotting and show plot.
-lw = 3
-lineGray, = ax.plot(np.arange(bins), np.zeros((bins,1)), c='k', lw=lw, label='intensity')
+# Initialize plot line object
+lineGray, = ax.plot(np.arange(bins), np.zeros((bins,1)), c='k', lw=3, label='Intensity')
+
+# Set axis limits
 ax.set_xlim(0, bins-1)
 ax.set_ylim(0, 1)
+
+# Show the plot legend
 ax.legend()
+
+# Turn on interactive plotting and show plot
 plt.ion()
 plt.show()
 
+# Check the camera
 if not cap.isOpened():
     print("Cannot open camera")
     exit()
@@ -44,19 +48,19 @@ if not cap.isOpened():
 while True:
     ret, frame = cap.read()
 
-    # if frame is read correctly ret is True
+    # If frame is read correctly ret is True
     if not ret:
         print("Can't receive frame.")
         break
 
-    # Resize frame to width, if specified.
+    # Resize frame to width if wanted
     if resizeWidth > 0:
         (height, width) = frame.shape[:2]
         resizeHeight = int(float(resizeWidth / width) * height)
         frame = cv2.resize(frame, (resizeWidth, resizeHeight),
             interpolation=cv2.INTER_AREA)
 
-    # Normalize histograms based on number of pixels per frame.
+    # Normalize histograms based on number of pixels per frame
     numPixels = np.prod(frame.shape[:2])
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     cv2.imshow('frame', gray)
