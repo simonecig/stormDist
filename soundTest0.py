4df55ce0c1c2bff83e4e24132204048ae0b4c9d3
simonecig <simonecigagna@gmail.com>
2020-07-27 14:17:47 +0200

    text spectrogram

--- soundTest0.py
+++ soundTest0.py
@@ -1,39 +1,110 @@
-import pyaudio
-import struct
-import keyboard
-import librosa as libr
-import librosa.display
-import cv2
-import matplotlib.pyplot as plt
+"""Show a text-mode spectrogram using live microphone data."""
+import argparse
+import math
+import shutil
+
 import numpy as np
-from scipy import signal
+import sounddevice as sd
 
-mic = pyaudio.PyAudio()
-FORMAT = pyaudio.paFloat32
-CHANNELS = 1
-RATE = 48000
-CHUNK = 1024 * 50
+usage_line = ' press <enter> to quit, +<enter> or -<enter> to change scaling '
 
-stream = mic.open(format=FORMAT, channels=CHANNELS, rate=RATE,
-                  input=True, output=False, frames_per_buffer=CHUNK)
 
-plt.ylabel('Frequency [Hz]')
-plt.xlabel('Time [sec]')
-while True:
-    data = stream.read(CHUNK, exception_on_overflow=False)
-    data_np = np.frombuffer(data, dtype=np.float32)
-    plt.clf()
-    hop_length = 1024
-    D = libr.amplitude_to_db(np.abs(libr.stft(data_np,
-                                              hop_length=hop_length)),
-                             ref=np.max)
-    libr.display.specshow(D, y_axis='log', sr=RATE, hop_length=hop_length,
-                          x_axis='time')
-    plt.colorbar(format="%+2.f dB")
-    plt.pause(0.001)
-    if keyboard.is_pressed('q'):
-        break
+def int_or_str(text):
+    """Helper function for argument parsing."""
+    try:
+        return int(text)
+    except ValueError:
+        return text
 
-stream.stop_stream()
-stream.close()
-mic.terminate()
+
+try:
+    columns, _ = shutil.get_terminal_size()
+except AttributeError:
+    columns = 80
+
+parser = argparse.ArgumentParser(add_help=False)
+parser.add_argument(
+    '-l', '--list-devices', action='store_true',
+    help='show list of audio devices and exit')
+args, remaining = parser.parse_known_args()
+if args.list_devices:
+    print(sd.query_devices())
+    parser.exit(0)
+parser = argparse.ArgumentParser(
+    description=__doc__ + '\n\nSupported keys:' + usage_line,
+    formatter_class=argparse.RawDescriptionHelpFormatter,
+    parents=[parser])
+parser.add_argument(
+    '-b', '--block-duration', type=float, metavar='DURATION', default=50,
+    help='block size (default %(default)s milliseconds)')
+parser.add_argument(
+    '-c', '--columns', type=int, default=columns,
+    help='width of spectrogram')
+parser.add_argument(
+    '-d', '--device', type=int_or_str,
+    help='input device (numeric ID or substring)')
+parser.add_argument(
+    '-g', '--gain', type=float, default=10,
+    help='initial gain factor (default %(default)s)')
+parser.add_argument(
+    '-r', '--range', type=float, nargs=2,
+    metavar=('LOW', 'HIGH'), default=[100, 2000],
+    help='frequency range (default %(default)s Hz)')
+args = parser.parse_args(remaining)
+low, high = args.range
+if high <= low:
+    parser.error('HIGH must be greater than LOW')
+
+# Create a nice output gradient using ANSI escape sequences.
+# Stolen from https://gist.github.com/maurisvh/df919538bcef391bc89f
+colors = 30, 34, 35, 91, 93, 97
+chars = ' :%#\t#%:'
+gradient = []
+for bg, fg in zip(colors, colors[1:]):
+    for char in chars:
+        if char == '\t':
+            bg, fg = fg, bg
+        else:
+            gradient.append('\x1b[{};{}m{}'.format(fg, bg + 10, char))
+
+try:
+    samplerate = sd.query_devices(args.device, 'input')['default_samplerate']
+
+    delta_f = (high - low) / (args.columns - 1)
+    fftsize = math.ceil(samplerate / delta_f)
+    low_bin = math.floor(low / delta_f)
+
+    def callback(indata, frames, time, status):
+        if status:
+            text = ' ' + str(status) + ' '
+            print('\x1b[34;40m', text.center(args.columns, '#'),
+                  '\x1b[0m', sep='')
+        if any(indata):
+            magnitude = np.abs(np.fft.rfft(indata[:, 0], n=fftsize))
+            magnitude *= args.gain / fftsize
+            line = (gradient[int(np.clip(x, 0, 1) * (len(gradient) - 1))]
+                    for x in magnitude[low_bin:low_bin + args.columns])
+            print(*line, sep='', end='\x1b[0m\n')
+        else:
+            print('no input')
+
+    with sd.InputStream(device=args.device, channels=1, callback=callback,
+                        blocksize=int(samplerate * args.block_duration / 1000),
+                        samplerate=samplerate):
+        while True:
+            response = input()
+            if response in ('', 'q', 'Q'):
+                break
+            for ch in response:
+                if ch == '+':
+                    args.gain *= 2
+                elif ch == '-':
+                    args.gain /= 2
+                else:
+                    print('\x1b[31;40m', usage_line.center(args.columns, '#'),
+                          '\x1b[0m', sep='')
+                    break
+except KeyboardInterrupt:
+    parser.exit('Interrupted by user')
+except Exception as e:
+    parser.exit(type(e).__name__ + ': ' + str(e))
